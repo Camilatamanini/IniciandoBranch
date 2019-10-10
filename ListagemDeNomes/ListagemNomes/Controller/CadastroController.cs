ec7fdc62538034eb28c291ae3c27eaa75614aba9
Camila Tamanini <camilatamaninifraga@hotmail.com>
2019-10-10 17:14:58 -0300

    commitando

---
+++ ListagemDeNomes/ListagemNomes/Controller/CadastroController.cs
@@ -0,0 +1,37 @@
+﻿using ListagemNomes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListagemNomes.Controller
+{
+    public class CadastroController
+    {
+       static CadastroContext contextDB = new CadastroContext();
+
+        /// <summary>
+        /// Método que acessa nosso banco de dados juntamente com a tabela e filtros.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<Cadastro> GetCadastros()
+        {
+            return contextDB
+                .ListaDeCadastros
+                .Where(x => x.Ativo == true);
+        }
+        public bool AdicionarNomes (Cadastro parametroCadastro)
+        {
+            if (string
+                .IsNullOrWhiteSpace
+                (parametroCadastro.Nome))
+                return false;
+
+            contextDB.ListaDeCadastros.Add(parametroCadastro);
+            contextDB.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ ListagemDeNomes/ListagemNomes/Model/CadastroContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListagemNomes.Model
+{
+     class CadastroContext : DbContext
+     {
+        public DbSet<Cadastro> ListaDeCadastros { get; set; }
+     }
+}
